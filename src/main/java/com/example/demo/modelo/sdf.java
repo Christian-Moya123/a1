df3edc427c18fe9828104d0710fd386f15f1194f
Christian Moya <cmoyacharro@gamil.com>
2022-09-09 08:08:35 -0500

    prueba1

---
+++ src/main/java/com/example/demo/modelo/sdf.java
@@ -0,0 +1,12 @@
+package com.example.demo.modelo;
+
+import javax.persistence.Id;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class sdf {
+	
+	@Id
+	private int id;
+}
